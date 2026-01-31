e1499bd553d18cadbc6b51c20d0fbb57c47914d3
rizwan99567 <rizwan99567@gmail.com>
2026-01-31 20:56:15 +0530

    Add files via upload

---
+++ sencond.py
@@ -0,0 +1,13 @@
+line = "+------+----------+--------+"
+
+print(line)
+print("| {:^4} | {:^8} | {:^6} |".format("roll", "name", "marks"))
+print(line)
+
+print("| {:^4} | {:^8} | {:^6} |".format(1, "Rizwan", 88))
+print(line)
+print("| {:^4} | {:^8} | {:^6} |".format(2, "Sufiyan", 77))
+print(line)
+print("| {:^4} | {:^8} | {:^6} |".format(3, "Khizar", 90))
+print(line)
+
